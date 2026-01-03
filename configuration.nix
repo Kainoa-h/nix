994037d6254b2194bbc52be19cda049a74850526
kainoa-h <kainoahowj@gmail.com>
2026-01-03 15:38:00 +0800

    feat(home-manager): added home manager

--- configuration.nix
+++ configuration.nix
@@ -2,12 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
     ];
 
   # Bootloader.
@@ -54,6 +55,13 @@
     description = "kai";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
+  };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "kai" = import ./home.nix;
+    };
   };
 
   # Allow unfree packages
