40d1660aa852a6589a4f5cdc1f68a138e74a2e3b
kainoa-h <kainoahowj@gmail.com>
2026-02-23 10:31:54 +0800

    feat(virtualisation): add podman

--- modules/system/services/virtualization.nix
+++ modules/system/services/virtualization.nix
@@ -8,10 +8,19 @@
     dockerCompat = true;
     # Enable podman socket (for docker-compose compatibility)
     dockerSocket.enable = true;
+    defaultNetwork.settings.dns_enabled = true;
   };
+
+  #  Tell nixos to pass down delegates to users so k3d can use them
+  systemd.services."user@".serviceConfig.Delegate = "memory pids cpu cpuset io";
 
   # Install podman-compose
   environment.systemPackages = with pkgs; [
     podman-compose
+    k3d
+    kubectl
+    k9s
   ];
+
+  users.users.kai.extraGroups = [ "podman" ];
 }
