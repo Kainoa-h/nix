0b8241b07675b61e086cbfc35c918045a6fb0a20
kainoa-h <kainoahowj@gmail.com>
2026-02-24 13:57:27 +0800

    feat(cache): add bin cache to nixos and darwin

--- modules/system/darwin/nix.nix
+++ modules/system/darwin/nix.nix
@@ -6,7 +6,19 @@
   nixpkgs.config.allowUnfree = true;
 
   # Enable experimental features
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://numtide.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    ];
+  };
 
   # Auto-optimize nix store
   # nix.optimise.automatic = true;
