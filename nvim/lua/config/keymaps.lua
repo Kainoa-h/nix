b1a24dc2dcfd1db85a0a7a0df4ec94dbaad675d6
kainoa-h <kainoahowj@gmail.com>
2026-03-14 00:29:56 +0800

    feat(nvim): add codecompanion

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -46,3 +46,12 @@ vim.keymap.set("n", "<C-S-k>", "<cmd>cprev<CR>")
 vim.keymap.set("n", "<leader>b/", function()
   require("telescope.builtin").current_buffer_fuzzy_find()
 end, { desc = "Fuzzy find in current buffer" })
+
+-- Code Companion
+vim.keymap.del("n", "ga")
+vim.keymap.del("v", "ga")
+vim.keymap.set("n", "ga", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
