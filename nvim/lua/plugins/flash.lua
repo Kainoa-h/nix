765f828a1828c6d81adee0437b076f9d4fede4dd
kainoa-h <kainoahowj@gmail.com>
2026-03-22 23:06:40 +0800

    fix(nvim): flash binds

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        char = {
+          enabled = true,
+          jump_labels = true,
+          multi_line = true,
+        },
+      },
+    },
+  },
+}
