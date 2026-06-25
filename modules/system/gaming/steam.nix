6db0b195a821f38709c305f6eafc4a359a6aa942
kainoa-h <kainoahowj@gmail.com>
2026-06-25 22:19:08 +0800

    feat: steam shit

--- modules/system/gaming/steam.nix
+++ modules/system/gaming/steam.nix
@@ -6,5 +6,8 @@
     enable = true;
     remotePlay.openFirewall = true; # Opens ports for Remote Play
     dedicatedServer.openFirewall = true; # Opens ports for dedicated servers
+    extest.enable = true;
   };
+
+  programs.gamescope.enable = true;
 }
