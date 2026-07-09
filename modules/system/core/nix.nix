5ca912e0f387e57cd969035c27ba213fea6977da
Kainoa-h <kainoahowj@gmail.com>
2026-07-09 08:18:24 +0800

    feat(nix): add numtide's new binary cache

--- modules/system/core/nix.nix
+++ modules/system/core/nix.nix
@@ -10,15 +10,15 @@
   # Nix daemon settings
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
-    substituters = [
-      "https://cache.nixos.org/"
+    extra-substituters = [
       "https://nix-community.cachix.org"
       "https://numtide.cachix.org"
+      "https://cache.numtide.com"
     ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     ];
   };
 }
